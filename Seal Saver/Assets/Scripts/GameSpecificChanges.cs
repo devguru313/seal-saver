7da44b7cd81c3c6f05fecbba4fd292f5a893e348
Harsh Gupta <harshgupt91@gmail.com>
2018-07-13 17:03:57 +0800

    Fixed animation so that it is not cumulative anymore by changing animation to non-root motion mode

--- Seal Saver/Assets/Scripts/GameSpecificChanges.cs
+++ Seal Saver/Assets/Scripts/GameSpecificChanges.cs
@@ -14,7 +14,7 @@ public class GameSpecificChanges : MonoBehaviour {
     private void Awake()
     {
         gameName = "Seal Saver";
-        version = "v0.97";
+        version = "v0.98";
     }
 
     private void Update()
